0cd20aad8a74cc23a0e2d1c7f50a28827cd7d4c0
philcal <philip.callender@tooltwist.com>
2016-09-14 16:01:10 +0800

    Initial Commit

---
+++ deploymentMonster/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  deploymentMonster
+//
+//  Created by Philip Callender on 14/09/2016.
+//  Copyright © 2016 Philip Callender. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ deploymentMonster/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  deploymentMonster
+//
+//  Created by Philip Callender on 14/09/2016.
+//  Copyright © 2016 Philip Callender. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
